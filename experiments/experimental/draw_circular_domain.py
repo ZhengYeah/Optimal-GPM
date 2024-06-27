220146d1b952ceb3a080a20e17c6fce91f5cd570
ZhengYeah <zhenghyeah.foxmail.com>
2024-06-27 16:28:41 -0400

    Finished figures

--- experiments/experimental/draw_circular_domain.py
+++ experiments/experimental/draw_circular_domain.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib.ticker import FormatStrFormatter, FuncFormatter
+from matplotlib.ticker import FormatStrFormatter
 
 
 plt.rcParams['text.usetex'] = True
